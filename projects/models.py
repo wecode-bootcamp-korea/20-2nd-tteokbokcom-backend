3993f287a680f0fc5010b1500d5b6eb827138d4d
이승무 <test850317@gmail.com>
2021-05-24 20:04:53 +0900

    Add: 프로젝트 모델 작성

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class Project(models.Model):
+    title           = models.CharField(max_length=100)
+    creater         = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    summary         = models.TextField()
+    category        = models.ForeignKey("Category", on_delete=models.CASCADE)
+    title_image_url = models.URLField(max_length=2000)
+    target_fund     = models.DecimalField(max_digits=10, decimal_places=2)
+    launch_data     = models.DateTimeField()
+    end_data        = models.DateTimeField()
+    created_at      = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "projects"
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
