93d4649a1c55dff391948ccd53f88f95b7e9b4ec
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-24 18:26:39 +0900

    ADD: 이메일 회원가입 / 로그인 기능 + 카카오 소셜 로그인 기능 구현
    - Users App 추가
    - Settings에 JWT 관련 세팅 추가
    - /signin URL 추가
    - 회원가입 관련 auth.py util 추가
    - SignInView 로직 구현
    - 카카오 소셜 로그인 URL 추가
    - KakaoSignInView 로직 구현
    - 카카오 로그인시 기존 이메일 가입자 예외 처리 추가
    - 카카오 REST API 예외 처리 추가
    - SignIn View 테스트 코드 추가
    - 카카오 로그인 테스트 케이스 추가

--- utils/auth.py
+++ utils/auth.py
@@ -1,7 +1,9 @@
 import jwt
+import time
+import bcrypt
 
 from users.models       import User
-from tteokbok.settings  import JWT_SECRET_KEY, JWT_ALGORITHM
+from tteokbok.settings import JWT_SECRET_KEY, JWT_ALGORITHM, JWT_DURATION_SEC
 
 def decode_jwt(token):
     return jwt.decode(token, JWT_SECRET_KEY, JWT_ALGORITHM)
@@ -10,3 +12,29 @@ def get_user_from_jwt(token):
     payload = decode_jwt(token)
     user    = User.objects.get(pk=payload.get('user_id'))
     return user
+
+class UnauthorizationError(Exception):
+    def __init__(self, err_msg = None):
+        super().__init__()
+        self.err_message = err_msg
+
+def check_password(password, user):
+    if not bcrypt.checkpw(password.encode('utf-8'),user.password.encode('utf-8')):
+        raise UnauthorizationError("Wrong Password Entered.")
+    
+    return password
+
+def hash_password(password):
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+def issue_token(user):
+    new_token = jwt.encode(
+                            {
+                                'user_id': user.id,
+                                'iat'    : int(time.time()),
+                                'exp'    : int(time.time()) + JWT_DURATION_SEC
+                            }, 
+                            JWT_SECRET_KEY, 
+                            JWT_ALGORITHM
+                            )
+    return new_token
