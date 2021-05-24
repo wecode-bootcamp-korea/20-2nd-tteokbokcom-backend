4052e0dc39ed9d4c13d2dab19261ab17e4c10809
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-24 18:26:39 +0900

    CREATE: projects / users APP Model  - projects / users APP Model 작성  - settings.py apps 리스트 추가

---
+++ users/models.py
@@ -0,0 +1,30 @@
+from django.db              import models
+from django.forms.models    import model_to_dict
+
+class User(models.Model):
+
+    username          = models.CharField(max_length=40, blank=False)
+    introduction      = models.TextField(null=True)
+    email             = models.EmailField(max_length=254, unique=True, blank=False)
+    password          = models.CharField(max_length=65, blank=False)
+    profile_image_url = models.URLField(max_length=2000)
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return f'{self.id}: {self.email} | {self.username}'
+
+    def to_dict(self, *args):
+        return model_to_dict(self, exclude=[*args])
+        
+class Likes(models.Model):
+
+    user    = models.ForeignKey('User', on_delete=models.CASCADE)
+    project = models.ForeignKey('projects.Project', on_delete=models.CASCADE)
+
+    class Meta():
+        db_table = 'likes'
+
+    def __str__(self):
+        return f'{self.user.username} : {self.project.title}'
