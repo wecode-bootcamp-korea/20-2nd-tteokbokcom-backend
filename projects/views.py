ef26ad3ad1df4de7d71ba8a72fa9c8555ac3901d
이승무 <test850317@gmail.com>
2021-05-26 14:05:40 +0900

    ADD: ProjectListView 구현
    - Filter / Sorting / Search 기능 구현
    - URL 설정 등록
    - 테스트케이스 작성

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,14 @@
 import json
 from json.decoder                   import JSONDecodeError
+from datetime                       import datetime
+from decimal                        import Decimal
 
 from django.views                   import View
-from django.db.models               import Sum
+from django.utils.decorators        import method_decorator
+from django.db.models               import Sum, F, Value, When, Case
 from django.http.response           import JsonResponse
+from django.db.models.functions     import Coalesce
+from django.db.models.aggregates    import Count
 from django.utils.decorators        import method_decorator
 
 from projects.models                import Category, Project, Tag, FundingOption
@@ -63,3 +68,73 @@ class ProjectDetailView(View):
 
         except Project.DoesNotExist:
             return JsonResponse({"status": "INVALID_PROJECT_ERROR", 'messages': 'Project does not exist.'}, status=404)
+
+class ProjectListView(View):
+    @method_decorator(check_user())
+    def get(self, request):
+        queries       = request.GET
+        user          = request.user
+        progress_min  = queries.get('progressMin')
+        progress_max  = queries.get('progressMax')
+        amount_min    = queries.get('amountMin')
+        amount_max    = queries.get('amountMax')
+        category      = queries.get('category')
+        status        = queries.get('status')
+        liked         = queries.get('liked')
+        sort_criteria = queries.get('sorted', 'default')
+        search        = queries.get('search')
+
+        filter_set = {
+            'progress__gte'      : progress_min,
+            'progress__lte'      : progress_max,
+            'funding_amount__gte': amount_min,
+            'funding_amount__lte': amount_max,
+            'category__name'     : category,
+            'status'             : status,
+            'is_liked'           : True if liked is not None else False,
+        }
+
+        filter_set = { k: v for k, v in filter_set.items() if v }
+
+        sortby_set = {
+            'default': '-created_at',
+            'latest' : '-created_at',
+            'people' : '-funding_count',
+            'amount' : '-funding_amount',
+            'old'    : 'end_date'
+        }
+
+        project_list = Project.objects.all().select_related('category', 'creater')\
+                                            .prefetch_related('donation_set', 'likes_set')\
+                                            .annotate(funding_amount = Coalesce(Sum('donation__funding_option__amount'), Decimal(0)))\
+                                            .annotate(funding_count = Coalesce(Count('donation'), 0))\
+                                            .annotate(progress = 100 * F('funding_amount')/F('target_fund'))\
+                                            .annotate(status = Case(When(end_date__lt = datetime.now(), then=Value("done")),
+                                                                    When(launch_date__gt = datetime.now(), then=Value("scheduled")),
+                                                                    default=Value("ing")))\
+                                            .annotate(is_liked = Case(When(likes__user = user, then=Value(True)),
+                                                                      default=Value(False)))\
+                                            .filter(**filter_set)\
+                                            .order_by(sortby_set[sort_criteria])
+
+        if search:
+            project_list = project_list.filter(title__contains=search) | project_list.filter(summary__contains=search)
+
+        projects = [{
+            'id'             : project.id,
+            'title_image_url': project.title_image_url,
+            'title'          : project.title,
+            'category'       : project.category.name,
+            'creater'        : project.creater.username,
+            'summary'        : project.summary,
+            'funding_amount' : float(project.funding_amount),
+            'funding_count'  : project.funding_count,
+            'target_amount'  : float(project.target_fund),
+            'launch_date'    : project.launch_date,
+            'end_date'       : project.end_date,
+            'status'         : project.status,
+            'progress'       : project.progress,
+            'is_liked'       : project.is_liked if user else False,
+        } for project in project_list]
+
+        return JsonResponse({'status': "SUCCESS", "data": {'num_projects': len(projects), 'projects': projects} }, status=200)
