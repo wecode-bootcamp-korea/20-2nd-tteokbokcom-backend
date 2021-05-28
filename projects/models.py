33e049ad85463fbde9bab3b6c9b22d59b855ddbe
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-28 19:55:37 +0900

    MODIFY: 모델 파일 수정
    - MODIFY: User 모델에 kakao_id 추가
    - MODIFY: 기타 코드컨벤션 수정

--- projects/models.py
+++ projects/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Project(models.Model):
-    title           = models.CharField(max_length=100)
-    creater         = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    summary         = models.TextField()
-    category        = models.ForeignKey("Category", on_delete=models.CASCADE)
-    title_image_url = models.URLField(max_length=2000)
-    target_fund     = models.DecimalField(max_digits=10, decimal_places=2)
-    launch_date     = models.DateTimeField()
-    end_date        = models.DateTimeField()
-    created_at      = models.DateTimeField(auto_now_add=True)
-    tag             = models.ManyToManyField("Tag", through="ProjectTag")
+    title                = models.CharField(max_length=100)
+    creater              = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    summary              = models.TextField()
+    category             = models.ForeignKey("Category", on_delete=models.CASCADE)
+    title_image_url      = models.URLField(max_length=2000)
+    target_fund          = models.DecimalField(max_digits=10, decimal_places=2)
+    launch_date          = models.DateTimeField()
+    end_date             = models.DateTimeField()
+    created_at           = models.DateTimeField(auto_now_add=True)
+    tag                  = models.ManyToManyField("Tag", through="ProjectTag")
 
     class Meta:
         db_table = "projects"
@@ -48,11 +48,11 @@ class FundingOption(models.Model):
         db_table = "funding_options"
 
 class Donation(models.Model):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    user           = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    project        = models.ForeignKey("Project", on_delete=models.CASCADE)
     funding_option = models.ForeignKey("FundingOption", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at     = models.DateTimeField(auto_now_add=True)
+    updated_at     = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "donations"
