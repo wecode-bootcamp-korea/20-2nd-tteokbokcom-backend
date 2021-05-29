2c18ae9c196183cf9d5bdab22bfeb45faa443eac
Jun-Sik Choi <leichoi91@gmail.com>
2021-05-29 19:23:03 +0900

    ADD: 사용자 정보 조회 MeView 구현
    - ADD: 로그인 체크 데코레이터 및 헬퍼 함수
    - ADD: MeView 로직구현
    - ADD: /me endpoint 추가
    - ADD: /users/me 테스트 코드 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http                import JsonResponse
+from django.views               import View
+from django.utils.decorators    import method_decorator
 
-# Create your views here.
+from utils.decorators        import login_required
+
+class MeView(View):
+    @method_decorator(login_required())
+    def get(self, request):
+        user = request.user
+        return JsonResponse({"status": "SUCCESS", "data": {"user": user.to_dict('password')}}, status=200)
