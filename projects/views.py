f703d6e499b8d62157ec0826ac1c4ee625221b52
Jun-Sik Choi <leichoi91@gmail.com>
2021-06-02 15:13:24 +0900

    ADD: Project List Veiw Filters

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,13 @@
-import json
-from json.decoder         import JSONDecodeError
+from datetime                   import datetime
+from decimal                    import Decimal
 
-from django.views         import View
-from django.db.models     import Sum
-from django.http.response import JsonResponse
+from django.views               import View
+from django.db.models           import Sum, F, Value, When, Case
+from django.http.response       import JsonResponse
+from django.db.models.functions import Coalesce
+from django.db.models.aggregates import Count
 
-from projects.models              import Project, Donation
+from projects.models            import Project
 
 class ProjectDetailView(View):
     def get(self, request, id):
@@ -42,20 +44,66 @@ class ProjectDetailView(View):
             return JsonResponse({'messages': 'DOES_NOT_EXIST'}, status=404)
 
 class ProjectListView(View):
-
+    
     def get(self, request):
-        project_list = Project.objects.all().order_by('-id').select_related('category', 'creater').prefetch_related('donation_set')
+        queries = request.GET
+        progress_min  = queries.get('progressMin')
+        progress_max  = queries.get('progressMax')
+        amount_min    = queries.get('amountMin')
+        amount_max    = queries.get('amountMax')
+        category      = queries.get('category')
+        status        = queries.get('status')
+        sort_criteria = queries.get('sorted', 'default')
+        search        = queries.get('search')
+
+        filter_set = {
+            'progress__gte'      : progress_min,
+            'progress__lte'      : progress_max,
+            'funding_amount__gte': amount_min,
+            'funding_amount__lte': amount_max,
+            'category__name'     : category,
+            'status'             : status
+        }
+
+        filter_set = { k: v for k, v in filter_set.items() if v }
+
+        sortby_set = {
+            'default': '-created_at',
+            'latest' : '-created_at',
+            'people' : '-funding_count',
+            'amount' : '-funding_amount',
+            'old'    : 'end_date'
+        }
+
+        project_list = Project.objects.all().order_by('-id')\
+                                            .select_related('category', 'creater')\
+                                            .prefetch_related('donation_set')\
+                                            .annotate(funding_amount = Coalesce(Sum('donation__funding_option__amount'), Decimal(0)))\
+                                            .annotate(funding_count = Coalesce(Count('donation'), 0))\
+                                            .annotate(progress = 100 * F('funding_amount')/F('target_fund'))\
+                                            .annotate(status = Case(When(end_date__lt = datetime.now(), then=Value("done")),
+                                                                    When(launch_date__gt = datetime.now(), then=Value("scheduled")),
+                                                                    default=Value("ing")))
+
+        project_list = project_list.filter(**filter_set).order_by(sortby_set[sort_criteria])
+
+        if search:
+            project_list = project_list.filter(title__contains = search) | project_list.filter(summary__contains = search)
 
         projects = [{
-            'id'                   : project.id,
-            'title_image_url'      : project.title_image_url,
-            'title'                : project.title,
-            'category'             : project.category.name,
-            'creater'              : project.creater.username,
-            'summary'              : project.summary,
-            'funding_amount'       : float(project.donation_set.all().aggregate(Sum('funding_option__amount'))['funding_option__amount__sum'] if len(project.donation_set.all()) > 0 else 0),
-            'target_amount'        : int(project.target_fund),
-            'end_date'             : project.end_date
+            'id'             : project.id,
+            'title_image_url': project.title_image_url,
+            'title'          : project.title,
+            'category'       : project.category.name,
+            'creater'        : project.creater.username,
+            'summary'        : project.summary,
+            'funding_amount' : float(project.funding_amount),
+            'funding_count'  : project.funding_count,
+            'target_amount'  : float(project.target_fund),
+            'launch_date'    : project.launch_date,
+            'end_date'       : project.end_date,
+            'status'         : project.status,
+            'progress'       : project.progress,
         } for project in project_list]
 
-        return JsonResponse({'status': "SUCCESS", "data": {'projects': projects} }, status=200)
+        return JsonResponse({'status': "SUCCESS", "data": {'num_projects': len(projects), 'projects': projects} }, status=200)
